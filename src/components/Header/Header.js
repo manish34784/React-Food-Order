8dba13f28af56bb4e3892c4c2641c63e303ef133
manish34784 <manish34784@gmail.com>
2024-01-28 14:12:14 +0530

    Test commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,19 @@
-import React, { Fragment } from 'react'
-import './Header.css';
-import mealsImage from '../../assets/meals.jpeg'
-import HeaderCartButton from '../HeaderCartButton/HeaderCartButton';
+import React, { Fragment } from "react";
+import "./Header.css";
+import mealsImage from "../../assets/meals.jpeg";
+import HeaderCartButton from "../HeaderCartButton/HeaderCartButton";
 
 export default function Header(props) {
+  console.log("Header props: ", props);
   return (
     <Fragment>
-      <header className='header'>
+      <header className="header">
         <h2>Meals</h2>
         <HeaderCartButton openCart={props.openCart} />
       </header>
-      <div className='main-image'>
+      <div className="main-image">
         <img src={mealsImage} alt="Foods image" />
       </div>
     </Fragment>
-  )
+  );
 }
